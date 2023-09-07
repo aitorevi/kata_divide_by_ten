f06a3a67d42b8c9bdd18c5d647623bd1739d7498
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-09-08 00:33:11 +0200

    test(red): zero value

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,7 +1,22 @@
-import { saidHello} from "../core/example";
+/*
+Dado un número, vamos dividiendo entre 10 hasta que la parte entera del resultado sea 0 y retornamos
+el número de divisiones necesarias para llegar a ese dato.
 
-describe("Hello", () => {
-    it("world!", () => {
-        expect(saidHello("world")).toBe("Hello world!")
+(0) -> 0
+(1) -> 1
+(10) -> 2
+(-20) -> 2  " -0,2 si le quito la parte entera con Math.abs, que queda? 0? "
+const a = -0,2 // const b = Math.abs(Math.trunc(a))
+(24) -> 2
+("") -> Exception
+
+Utilizar recursividad
+ */
+
+import {saidHello} from "../core/example";
+
+describe("Divisions to zero", () => {
+    it("zero value", () => {
+        expect(divideByTen(0)).toBe(0)
     })
 })
